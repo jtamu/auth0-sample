05e9a17fa54be93fa46504f5379091e15cd4d7bd
jtamu <gtamu.dip@gmail.com>
2024-04-17 23:22:39 +0900

    feat: access_token -> id_token

--- app/api/microposts/route.js
+++ app/api/microposts/route.js
@@ -1,17 +1,15 @@
-import { getAccessToken, withApiAuthRequired } from '@auth0/nextjs-auth0';
+import { getSession, withApiAuthRequired } from '@auth0/nextjs-auth0';
 import { NextResponse } from 'next/server';
 
 export const GET = withApiAuthRequired(async function shows(req) {
   try {
     const res = new NextResponse();
-    const { accessToken } = await getAccessToken(req, res, {
-      scopes: ['read:microposts']
-    });
+    const { idToken } = await getSession(req, res);
     const apiPort = process.env.API_PORT || 8000;
     const apiHost = process.env.API_HOST;
     const response = await fetch(`${apiHost}:${apiPort}/auth0/microposts`, {
       headers: {
-        Authorization: `Bearer ${accessToken}`
+        Authorization: `Bearer ${idToken}`
       }
     });
     const shows = await response.json();
